ec25b069c14a44d9c67f522f6bbdac53485d5693
Blaž <blaz.rupnik56@gmail.com>
2020-01-09 10:19:23 +0100

    Added load test for hpa

---
+++ LoadTestingApp/LoadTestingApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+
+namespace LoadTestingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meant for scaling test of microservices running on kubernetes");
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            for(int i=0; i<10; i++)
+            {
+                DownloadUrl("https://35.197.13.255/comments/v1/comments");
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static string DownloadUrl(string url)
+        {
+            return new System.Net.WebClient().DownloadString(url);
+        }
+
+
+    }
+}
